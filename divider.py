73114452a2794cbde507198a706e89b1e1506cc1
JorgeA-Z <74442427+JorgeA-Z@users.noreply.github.com>
2024-11-25 08:35:32 -0600

    Update divider.py

--- divider.py
+++ divider.py
@@ -1,32 +1,32 @@
 import cv2
 
-# Abre el archivo de video
-video_capture = cv2.VideoCapture('1.mov')
+class Extractor():
+    def __init__(self, route):
+        self.route = route
+        self.frameCount = 0
 
-# Verifica si el archivo de video se abrió correctamente
-if not video_capture.isOpened():
-    print("Error al abrir el video")
-    exit()
+    def saveFrame(self, frame):
+        self.frameCount += 1
+        frame_filename = f'frames/frame_{self.frameCount}.jpg'
+        cv2.imwrite(frame_filename, frame)
 
-# Contador para llevar el seguimiento del número de frames
-frame_count = 0
+    def readFrames(self):
+        video = cv2.VideoCapture(fr'{self.route}')
+        if not video.isOpened():
+            print("Error al abrir el video")
+            exit()
+            
+        while True:
+            ret, frame = video.read()
+            if not ret:
+                break
 
-# Bucle para leer y guardar cada frame del video
-while True:
-    ret, frame = video_capture.read()
+            self.saveFrame(frame);
+        video.release()
 
-    if not ret:
-        break
+        print(f'Se extrajeron {self.frameCount} frames del video.')
 
-    # Aquí puedes procesar o guardar cada frame como desees
-    frame_count += 1
 
-    # Por ejemplo, puedes guardar el frame en un archivo de imagen
-    frame_filename = f'frame_{frame_count}.jpg'
-    cv2.imwrite(frame_filename, frame)
-
-# Cierra la captura de video
-video_capture.release()
-
-print(f'Se extrajeron {frame_count} frames del video.')
-
+if __name__ == "__main__":
+    extractor = Extractor(r'C:\Users\jorge_zepeda\Desktop\Extractor-de-frames\Flujo.mp4')
+    extractor.readFrames()
